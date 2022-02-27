e2d1f6eda0963716660497d16f1ffd9a6a7af54d
Ben Dyer <ben@fifthhour.co.uk>
2022-02-27 09:00:25 +0000

    add domain class

---
+++ src/headwaters/domains/domain.py
@@ -0,0 +1,11 @@
+class Domain:
+
+    """this is a class that:
+    
+    reads info from a json file into memory
+
+    
+    have get_event method to supply a new event
+    """
+
+    data = ["avocado", "banana", "cherry", "dates", "elderberries"]
